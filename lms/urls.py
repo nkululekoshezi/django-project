454368bd01fbc9a4fda58940341d6d6a5aec1eee
Shezi <nkululeko0201@gmail.com>
2022-10-09 03:10:46 +0200

    Initial commit

---
+++ lms/urls.py
@@ -0,0 +1,9 @@
+from lms import views
+from django.contrib import admin
+from django.urls import path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home,name='home-page'),
+]
